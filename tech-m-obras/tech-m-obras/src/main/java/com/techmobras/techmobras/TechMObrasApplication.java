358d98210c468f33990f6fdf6cf4410a424b8e49
anajuliaflx <anajuliafdoliveira@gmail.com>
2022-11-12 16:38:25 -0300

    adicionando a estrutura springboot

---
+++ tech-m-obras/tech-m-obras/src/main/java/com/techmobras/techmobras/TechMObrasApplication.java
@@ -0,0 +1,13 @@
+package com.techmobras.techmobras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechMObrasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechMObrasApplication.class, args);
+	}
+
+}
---
+++ tech-m-obras/tech-m-obras/src/test/java/com/techmobras/techmobras/TechMObrasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techmobras.techmobras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechMObrasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
