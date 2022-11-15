b75c9f34042a8c16e77bfda376c60b8e1ceab813
vngoliveira <vivi40195@gmail.com>
2022-11-15 01:15:33 -0300

    created class ObraLocalizacao

---
+++ tech-m-obras/tech-m-obras/src/main/java/com/techmobras/techmobras/model/ObraLocalizacao.java
@@ -0,0 +1,93 @@
+package com.techmobras.techmobras.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "obra_localizacao")
+public class ObraLocalizacao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 45, nullable = false)
+    private String estado;
+
+    @Column(length = 45, nullable = false)
+    private String cidade;
+
+    @Column(length = 45, nullable = false)
+    private String latitude;
+
+    @Column(length = 45, nullable = false)
+    private String longitude;
+
+    @OneToMany
+    @JoinColumn(nullable = false)
+    private Obra obraId;
+
+    public ObraLocalizacao(Long id, String estado, String cidade, String latitude, String longitude, Obra obraId) {
+        this.id = id;
+        this.estado = estado;
+        this.cidade = cidade;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.obraId = obraId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public Obra getObraId() {
+        return obraId;
+    }
+
+    public void setObraId(Obra obraId) {
+        this.obraId = obraId;
+    }
+
+    
+}
