3a9de1c20906bfb01b2cd4d68496af0a1032488b
vngoliveira <vivi40195@gmail.com>
2022-11-15 16:18:54 -0300

    created class ObraLocalizacaoDTO

---
+++ tech-m-obras/tech-m-obras/src/main/java/com/techmobras/techmobras/dto/ObraLocalizacaoDTO.java
@@ -0,0 +1,35 @@
+package com.techmobras.techmobras.dto;
+
+public class ObraLocalizacaoDTO {
+    
+    public String getEstado() {
+        return estado;
+    }
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    public String getCidade() {
+        return cidade;
+    }
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+    public String getLongitute() {
+        return longitute;
+    }
+    public void setLongitute(String longitute) {
+        this.longitute = longitute;
+    }
+    public String getLatitude() {
+        return latitude;
+    }
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    private String estado;
+    private String cidade;
+    private String longitute;
+    private String latitude;
+
+}
