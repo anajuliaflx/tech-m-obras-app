62e2e9923a232006d1167c9d83ef30ba47940fbc
vngoliveira <vivi40195@gmail.com>
2022-11-17 05:46:42 -0300

    create ObraLocalizacaoController and write post method

---
+++ tech-m-obras/tech-m-obras/src/main/java/com/techmobras/techmobras/controller/ObraLocalizacaoController.java
@@ -0,0 +1,36 @@
+package com.techmobras.techmobras.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.techmobras.techmobras.dto.ObraLocalizacaoDTO;
+import com.techmobras.techmobras.model.ObraLocalizacao;
+import com.techmobras.techmobras.service.ObraLocalizacaoService;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/obra_localizacao")
+public class ObraLocalizacaoController {
+
+    final ObraLocalizacaoService obraLocalizacaoService;
+
+    public ObraLocalizacaoController(ObraLocalizacaoService obraLocalizacaoService) {
+        this.obraLocalizacaoService = obraLocalizacaoService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> saveObraLocalizacao(@RequestBody @Valid ObraLocalizacaoDTO obraLocalizacaoDTO) {
+        ObraLocalizacao obraLocalizacao = new ObraLocalizacao();
+        BeanUtils.copyProperties(obraLocalizacaoDTO, obraLocalizacao);
+        return ResponseEntity.status(HttpStatus.CREATED).body(obraLocalizacaoService.save(obraLocalizacao));
+    }
+
+}
