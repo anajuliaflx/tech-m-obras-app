2fc7df1e2ba8bfbb537e05213caca633414749c5
anajuliaflx <anajuliafdoliveira@gmail.com>
2022-11-18 01:48:32 -0300

    add annotation @Repository

--- tech-m-obras/tech-m-obras/src/main/java/com/techmobras/techmobras/repository/ObraDetalhesTecnicosRepository.java
+++ tech-m-obras/tech-m-obras/src/main/java/com/techmobras/techmobras/repository/ObraDetalhesTecnicosRepository.java
@@ -1,9 +1,11 @@
 package com.techmobras.techmobras.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.techmobras.techmobras.model.ObraDetalhesTecnicos;
 
+@Repository
 public interface ObraDetalhesTecnicosRepository extends JpaRepository<ObraDetalhesTecnicos, Integer> {
 
 }
