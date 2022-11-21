feeeae3979b9a7a08594454b2e0b71c6f3150a99
Rafael Tomás <rafaeltomassouza@gmail.com>
2022-11-20 23:28:14 -0300

    feat: create obra controller

--- tech-m-obras/tech-m-obras/src/main/java/com/techmobras/techmobras/controller/ObraController.java
+++ tech-m-obras/tech-m-obras/src/main/java/com/techmobras/techmobras/controller/ObraController.java
@@ -1,13 +1,76 @@
 package com.techmobras.techmobras.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.techmobras.techmobras.model.Obra;
+import com.techmobras.techmobras.repository.ObraRepository;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.*;
 
 @RestController
-@RequestMapping(value = "/obras")
+@RequestMapping("/api/obra")
 public class ObraController {
-  
 
+    private ObraRepository obraRepository;
 
+    public ObraController(ObraRepository obraRepository) {
+        this.obraRepository = obraRepository;
+    }
+
+    @GetMapping(value = "{id}")
+    @ResponseStatus(OK)
+    public Obra getObraById(@PathVariable int id) {
+        return obraRepository
+                .findById(id)
+                .orElseThrow(() ->
+                        new ResponseStatusException(HttpStatus.NOT_FOUND,
+                                "Obra não encontrada"));
+    }
+
+    @GetMapping
+    public List<Obra> find(Obra filtro) {
+        ExampleMatcher encontrar = ExampleMatcher
+                .matching()
+                .withIgnoreCase()
+                .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING);
+        Example exemplo = Example.of(filtro, encontrar);
+        return obraRepository.findAll(exemplo);
+    }
+
+    @PostMapping
+    @ResponseStatus(CREATED)
+    public Obra save(@RequestBody Obra obra) {
+        return obraRepository.save(obra);
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(NO_CONTENT)
+    public void delete(@PathVariable Integer id) {
+        obraRepository.findById(id)
+                .map(Obra -> {
+                    obraRepository.delete(Obra);
+                    return Void.TYPE;
+                }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "obra não encontrada"));
+    }
+
+    @PutMapping("{id}")
+    @ResponseStatus(NO_CONTENT)
+    public void update(@PathVariable Integer id,
+                       @RequestBody Obra obra) {
+        obraRepository
+                .findById(id)
+                .map(obraExistente -> {
+                    obra.setId(obraExistente.getId());
+                    obraRepository.save(obra);
+                    return obraExistente;
+                }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                        "obra não encontrada"));
+    }
 }
+
--- tech-m-obras/tech-m-obras/src/main/java/com/techmobras/techmobras/model/Obra.java
+++ tech-m-obras/tech-m-obras/src/main/java/com/techmobras/techmobras/model/Obra.java
@@ -4,6 +4,7 @@ import lombok.*;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 import java.math.BigInteger;
 
@@ -21,8 +22,7 @@ public class Obra {
     @Column(length = 45)
     private String nome;
 
-    @NotBlank
-    @Column
+    @NotNull
     private BigInteger anoConstrucao;
 
     @NotBlank
